816f3f6cedb7268269ce4a7c7503f35bdec6e2fc
Hoang-Minh <quataoxanh83@gmail.com>
2018-04-11 19:54:11 -0700

    update table

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
 
 // your code here...
-//var userArr = []
+var userArr = []
 
 // app.get('/getContact', function (req, res) {
 //   res.json(userArr)
@@ -30,15 +30,18 @@ app.get('/tables', function (req, res) {
     res.redirect('tables.html')
 })
 
-// app.post('/postContact', function (req, res) {
-//   userArr.push(req.body)
-//   res.json(userArr)
-//   // res.send(true)
-// })
+app.post('/api/tables', function (req, res) {
+  userArr.push(req.body)
+  res.json(userArr)
+  // res.send(true)
+})
 
-// app.get('/retrieveUsers', function (req, res) {
-//   res.json(userArr)
-// })
+app.get('/api/waitlist', function (req, res) {
+  res.json(userArr)
+})
+
+
+
 
 var PORT = process.env.PORT || 3000
 // listening port
